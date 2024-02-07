1dc60bda0dc52aab19e4dee297c3eb7dfbf7eea2
“uruya” <1ayawolf1@gmail.com>
2024-02-07 21:01:01 +0900

    学習

---
+++ app.js
@@ -0,0 +1,11 @@
+const PORT = process.env.PORT;
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use("/", require("./routes/index.js"));
+
+app.listen(PORT, ()=> {
+  console.log(`Application listening at ${PORT}`);
+});
