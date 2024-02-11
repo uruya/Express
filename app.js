5f848196318879052c5c1a0048d19060b3b840ac
“uruya” <1ayawolf1@gmail.com>
2024-02-11 17:27:44 +0900

    log作成

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const PORT = process.env.PORT;
 const path = require("path");
+const logger = require("./lib/log/logger.js");
+const accesslogger = require("./lib/log/accesslogger.js");
+const applicationlogger = require("./lib/log/applicationlogger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
 const app = express();
@@ -7,14 +10,20 @@ const app = express();
 // Express settings
 app.set("view engine", "ejs");
 app.disable("x-powered-by");
+
 // Static resource rooting
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
+// Set access log
+app.use(accesslogger());
 // Dynamic resource rooting
 app.use("/", require("./routes/index.js"));
 
+// Set application log.
+app.use(applicationlogger());
+
 // Execute web application
 app.listen(PORT, ()=> {
-  console.log(`Application listening at ${PORT}`);
+  logger.application.info(`Application listening at ${PORT}`);
 });
