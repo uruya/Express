182d97737325cb2576ed2249f6bb1be2be5ff00b
“uruya” <1ayawolf1@gmail.com>
2024-04-29 14:11:08 +0900

    cookie, session

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const PORT = process.env.PORT;
+const appconfig = require("./config/application.config.js");
+const dbconfig = require("./config/mysql.config.js");
 const path = require("path");
 const logger = require("./lib/log/logger.js");
 const accesslogger = require("./lib/log/accesslogger.js");
 const applicationlogger = require("./lib/log/applicationlogger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
+const cookie = require("cookie-parser");
+const session = require("express-session");
+const applicationConfig = require("./config/application.config.js");
+const MySQLStore = require("express-mysql-session")(session);
 const app = express();
 
 // Express settings
@@ -26,7 +31,22 @@ app.use("/public", express.static(path.join(__dirname, "/public")));
 app.use(accesslogger());
 
 // Set midleware
+app.use(cookie());
+app.use(session({
+  store: new MySQLStore({
+    host: dbconfig.HOST,
+    port: dbconfig.PORT,
+    user: dbconfig.USERNAME,
+    password: dbconfig.PASSWORD,
+    database: dbconfig.DATABASE
+  }),
+  secret: appconfig.secutirty.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  name: "sid"
+}));
 app.use(express.urlencoded({ extended: true }));
+
 
 // Dynamic resource rooting
 app.use("/account", require("./routes/account.js"));
@@ -38,6 +58,6 @@ app.use("/", require("./routes/index.js"));
 app.use(applicationlogger());
 
 // Execute web application
-app.listen(PORT, ()=> {
-  logger.application.info(`Application listening at ${PORT}`);
+app.listen(appconfig.PORT, ()=> {
+  logger.application.info(`Application listening at ${appconfig.PORT}`);
 });
--- config/application.config.js
+++ config/application.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  PORT: process.env.PORT || 3000,
+  secutirty: {
+    SESSION_SECRET: "YOUR_SESSION-SECRET-STRING"
+  },
   search: {
     MAX_ITEMS_PER_PAGE: 5
   }
