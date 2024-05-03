dbd5322705d46fc745700c25f52aabf8042c9704
“uruya” <1ayawolf1@gmail.com>
2024-05-03 15:13:28 +0900

    ログイン処理

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+const IS_PRODUCTION = process.env.NODE_ENV === "producttion";
 const appconfig = require("./config/application.config.js");
 const dbconfig = require("./config/mysql.config.js");
 const path = require("path");
 const logger = require("./lib/log/logger.js");
 const accesslogger = require("./lib/log/accesslogger.js");
 const applicationlogger = require("./lib/log/applicationlogger.js");
+const accesscontrol = require("./lib/security/accesscontrol.js");
 const express = require("express");
 const favicon = require("serve-favicon");
 const cookie = require("cookie-parser");
 const session = require("express-session");
-const applicationConfig = require("./config/application.config.js");
 const MySQLStore = require("express-mysql-session")(session);
+const flash = require("connect-flash");
 const app = express();
 
 // Express settings
@@ -40,13 +42,17 @@ app.use(session({
     password: dbconfig.PASSWORD,
     database: dbconfig.DATABASE
   }),
+  cookie: {
+    secure: IS_PRODUCTION
+  },
   secret: appconfig.secutirty.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
   name: "sid"
 }));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(flash());
+app.use(...accesscontrol.initialize());
 
 // Dynamic resource rooting
 app.use("/account", require("./routes/account.js"));
--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,16 @@
 const router = require("express").Router();
+const { authenticate, authorize, PRIVILEGE } = require("../lib/security/accesscontrol.js");
 
-router.use("/reviews", require("./account.reviews.js"));
+router.get("/", authorize(PRIVILEGE.NORMAL), (req, res) => {
+  res.render("./account/index.ejs");
+});
+
+router.get("/login", (req, res) => {
+  res.render("./account/login.ejs", { message: req.flash("message") });
+});
+
+router.post("/login", authenticate());
+
+router.use("/reviews", authorize(PRIVILEGE.NORMAL), require("./account.reviews.js"));
 
 module.exports = router;
