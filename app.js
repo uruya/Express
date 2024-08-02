6239633e2dc8598678ec29e8cdb856256b814fdb
“uruya” <1ayawolf1@gmail.com>
2024-08-02 20:57:03 +0900

    cookie対策

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(session({
   },
   secret: appconfig.security.SESSION_SECRET,
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   name: "sid"
 }));
 app.use(express.urlencoded({ extended: true }));
